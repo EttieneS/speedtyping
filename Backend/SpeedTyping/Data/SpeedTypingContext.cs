a68d4ec23431cd6f3ad0fbaa43286d1fa0565674
EttieneS <smithettiene@yahoo.com>
2020-10-10 15:58:54 +0200

    initial commit, proto backend

---
+++ Backend/SpeedTyping/Data/SpeedTypingContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SpeedTyping.Models;
+
+namespace SpeedTyping.Data
+{
+    public class SpeedTypingContext : DbContext
+    {
+        public SpeedTypingContext (DbContextOptions<SpeedTypingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SpeedTyping.Models.User> User { get; set; }
+    }
+}
