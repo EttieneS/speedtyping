7dacc75f313393b86e27c2a90082cbf3fdee37cb
EttieneS <smithettiene@yahoo.com>
2020-10-13 21:59:45 +0200

    going to bed, validation tomorrow ;)

--- Backend/SpeedTyping/Models/User.cs
+++ Backend/SpeedTyping/Models/User.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +10,19 @@ namespace SpeedTyping.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(15, ErrorMessage ="Competitor name may be no longer than 15 characters")]
         public string Name { get; set; }
+        public string LastName { get; set; }
+        [Timestamp]
+        public DateTime DateCreated { get; set; }
+        [Required]
+        [MaxLength(13)]
+        public int IdNumber { get; set; }
         public int Score { get; set; }
+        [DefaultValue(true)]
         public bool Competition { get; set; }
     }
 }
--- Frontend/SpeedTyping/src/app/app.module.ts
+++ Frontend/SpeedTyping/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { MatSliderModule } from '@angular/material/slider';
+import { MatSliderModule } from '@angular/material/slider';
 import { HomeComponent } from './home/home.component';
 import { GridUserComponent } from './grid-user/grid-user.component';
 import { AddOrUpdateUserComponent } from './add-or-update-user/add-or-update-user.component';
@@ -32,7 +32,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    //MatSliderModule,
+    MatSliderModule,
     RouterModule.forRoot(appRoutes),
     HttpClientModule,
     FormsModule,
