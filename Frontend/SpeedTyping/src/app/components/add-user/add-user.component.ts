a555c8d36491698be256833820f635d0282564e2
EttieneS <smithettiene@yahoo.com>
2020-10-20 20:39:44 +0200

    basic validation on add-user added

--- Frontend/SpeedTyping/src/app/components/add-user/add-user.component.ts
+++ Frontend/SpeedTyping/src/app/components/add-user/add-user.component.ts
@@ -30,22 +30,26 @@ export class AddUserComponent {
   ) {}
 
   ngOnInit(): void {
+    let cellphone_pattern = /[0-9\+\-\ ]/;
+
     this.userdetailsForm = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.maxLength(60)]),
-      lastName: new FormControl('', [Validators.required, Validators.maxLength(60)])
-      //lastName: new FormControl('', [Validators.required, Validators.maxLength(60)])
+      lastName: new FormControl('', [Validators.maxLength(60)]),
+      cellNumber: new FormControl('', [Validators.pattern("[0-9]{10}$")]),
+      idNumber: new FormControl('', [Validators.pattern("[0-9]{13}$")]),
+      dateOfBirth: new FormControl(new Date())
     });
   }
 
   saveUser(user): void {
     const data = {
       lastName: user.lastName,
-      //cellNumber: user.cellNumber,
-      // idNumber: this.user.idNumber,
-      // dateCreated: moment().tz("Africa/Johannesburg").format('yyyy-MM-DD'),
-      // dateOfBirth: moment(this.user.dateOfBirth).tz("Africa/Johannesburg").format('yyyy-MM-DD'),
-      // competition: true,
-      // score: Math.floor(Math.random() * 501),
+      cellNumber: user.cellNumber,
+      idNumber: user.idNumber,
+      dateCreated: moment().tz("Africa/Johannesburg").format('yyyy-MM-DD'),
+      dateOfBirth: moment(user.dateOfBirth).tz("Africa/Johannesburg").format('yyyy-MM-DD'),
+      competition: true,
+      score: Math.floor(Math.random() * 501),
       name: user.name
     }
 
